1b040d1d129ba82726ab8f85320785b05a4e6e5e
ralejandre <r111kytrigo@gmail.com>
2017-06-20 21:59:28 -0500

    In class todos

--- config/auth.js
+++ config/auth.js
@@ -1,7 +1,15 @@
+// module.exports = {
+//   'googleAuth': {
+//     'clientID'    : '794436202558-ojba52ljhd7bdqn5iss541jisq6453lq.apps.googleusercontent.com',
+//     'clientSecret': 'XHlS06Od6OtHR2wp2LHzOTX6',
+//     'callbackURL' : 'http://localhost:8080/ricardo/auth/google/callback'
+//   }
+// };
+
 module.exports = {
   'googleAuth': {
-    'clientID'    : '794436202558-ojba52ljhd7bdqn5iss541jisq6453lq.apps.googleusercontent.com',
-    'clientSecret': 'XHlS06Od6OtHR2wp2LHzOTX6',
-    'callbackURL' : 'http://localhost:8080/ricardo/auth/google/callback'
+    'clientID'    : 'CLIENTEID',
+    'clientSecret': 'CLIENTESECRET',
+    'callbackURL' : 'CALLBACKURL'
   }
 };
