0cc4e73bada3c009bd9929ed8546e01903598d0c
ralejandre <r111kytrigo@gmail.com>
2017-06-26 07:43:42 -0500

    hotfix

--- app/controllers/ladyController.js
+++ app/controllers/ladyController.js
@@ -2,9 +2,6 @@ var express = require('express')
 var router 	= express.Router()
 var passport	= require('passport')
 
-var gcal = require('google-calendar');
-var google_calendar   = require(__dirname +'/../../GoogleCalendar.js');
-
 
 module.exports = function(app){
 	app.use('/lady', router);
